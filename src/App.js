fb505161889a4fb1219b232cd459b16275c79031
b0c-beep <bocmarioadrian98@gmail.com>
2024-08-10 16:54:36 +0300

    deleted some unwanted stuff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
-//test
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          cock
-        </p>
-        
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
